92f351592b0543480fcb4131264dc3aa16d60584
Steve Pedersen <spedersen@spedersen18.dev.at.sfsu.edu>
2020-11-12 09:43:38 -0800

    default value added to function call

--- modules/Files/Controller.php
+++ modules/Files/Controller.php
@@ -42,7 +42,7 @@ class Classrooms_Files_Controller extends Classrooms_Master_Controller
         
         $fid = $this->getRouteVariable('id');
         $file = $this->requireExists($this->schema('Classrooms_Files_File')->get($fid));
-        $allowed = $this->getRouteVariable('allowed') ?? false;
+        $allowed = $this->getRouteVariable('allowed', false);
 
         if ($file->uploadedBy && !$allowed && ($account->id != $file->uploadedBy->id))
         {
