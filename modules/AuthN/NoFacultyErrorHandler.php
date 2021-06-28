d0f6b20b35abf1162859e2124d3d089e05aa9e59
Steve Pedersen <spedersen@spedersen18.dev.at.sfsu.edu>
2021-06-28 16:22:12 -0700

    Handler added

---
+++ modules/AuthN/NoFacultyErrorHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Workstation Selection application error handler for 404 errors.
+ * 
+ * @author      Charles O'Sullivan (chsoney@sfsu.edu)
+ * @copyright   Copyright &copy; San Francisco State University
+ */
+class Classrooms_AuthN_NoFacultyErrorHandler extends Classrooms_Master_ErrorHandler
+{
+    public static function getErrorClassList () { return array('Classrooms_AuthN_ExNoFaculty'); }
+    
+    protected function getStatusCode () { return 404; }
+    protected function getStatusMessage () { return 'No Faculty'; }
+    protected function getTemplateFile () { return 'error-nofaculty.html.tpl'; }
+}
